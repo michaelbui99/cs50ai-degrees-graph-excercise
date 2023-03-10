e5156df1830b51934efd1f694d2354f1c9459f6e
Michael Bui <michaelbui99@hotmail.com>
2023-03-11 00:00:51 +0100

    Refactor DataReader

--- data_reader.py
+++ data_reader.py
@@ -15,20 +15,14 @@ class DataReader:
         self.movies: pd.DataFrame
         self.people: pd.DataFrame
         self.stars: pd.DataFrame
-        if dataset.lower() not in supported_datasets:
+        ds = dataset.lower()
+        if ds not in supported_datasets:
             raise UnknownDataset(
                 "Uknown dataset provided. Please use 'large' or 'small'")
-        if (dataset.lower() == "small"):
-            self.movies = pd.read_csv(
-                f'{path}/small/movies.csv', sep=',')
-            self.people = pd.read_csv(
-                f'{path}/small/people.csv', sep=',')
-            self.stars = pd.read_csv(
-                f'{path}/small/stars.csv', sep=',')
-        else:
-            self.movies = pd.read_csv(
-                f'{path}/large/movies.csv', sep=',')
-            self.people = pd.read_csv(
-                f'{path}/large/people.csv', sep=',')
-            self.stars = pd.read_csv(
-                f'{path}/large/stars.csv', sep=',')
+
+        self.movies = pd.read_csv(
+            f'{path}/{ds}/movies.csv', sep=',')
+        self.people = pd.read_csv(
+            f'{path}/{ds}/people.csv', sep=',')
+        self.stars = pd.read_csv(
+            f'{path}/{ds}/stars.csv', sep=',')
